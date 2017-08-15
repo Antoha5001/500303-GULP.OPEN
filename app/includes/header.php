7eb5ac49827956272c479051f149fd2b280e1675
Antoha5001 <plantsvzzombi@mail.ru>
2017-08-15 17:58:24 +0700

    add pages

---
+++ app/includes/header.php
@@ -0,0 +1,39 @@
+<!Doctype html>
+<html lang="ru">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+    <meta name="keywords" content=""/>
+    <meta name="description" content=""/>
+    <meta name="author" content=""/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="css/mystyle.min.css">
+    <title>МЦ «ЖИЗНЬ»</title>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<div class="wrapper s-conteiner">
+    <header class="row">
+        <div class="col col-lg-6  col-md-12 logo-box">
+            <a href="index.php" class="logo-box__logo">
+                <img src="images/logo.svg" alt=""/>
+            </a>
+        </div>
+        <div class="col col-lg-6 col-md-12">
+            <div class="phone-top">
+                <p class="phone-top__title">МНОГОКАНАЛЬНЫЙ ТЕЛЕФОН</p>
+                <p class="phone-top__kod-goroda">+7 (3852) <span class="phone-top__kod-goroda phone-top__number">50-03-03</span>
+                </p>
+                <p class="phone-top__adres">
+                    г. Барнаул, ул.&nbsp;Партизанская,&nbsp;169</p>
+            </div>
+        </div>
+    </header>
